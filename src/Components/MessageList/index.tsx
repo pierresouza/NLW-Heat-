8f03bc04bdd9ff0f93e339d568323b40ed2c6592
pierre souza <pas@lionx.com.br>
2022-04-28 14:09:46 -0300

    Initial commit

---
+++ src/Components/MessageList/index.tsx
@@ -0,0 +1,48 @@
+import styles from './styles.module.scss'
+import { api } from '../../services/api'
+import logoImg from '../../assets/Logo DoWhile - 2021.svg'
+import { useEffect, useState } from 'react'
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
+export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data)
+    })
+  }, [])
+
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile2021" />
+
+      <ul className={styles.messageList}>
+        {messages.map(message => {
+          return (
+            <li className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          )
+        })}
+
+
+      </ul>
+    </div>
+  )
+}
