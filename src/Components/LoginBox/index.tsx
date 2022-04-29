cea8f2b305247e4f6a5398913686c45821bfb8c4
Pierre Souza <pas@lionx.com.br>
2022-04-29 10:22:28 -0300

    update

--- src/Components/LoginBox/index.tsx
+++ src/Components/LoginBox/index.tsx
@@ -1,19 +1,53 @@
-import { VscGithubInverted } from 'react-icons/vsc'
+import { func } from "prop-types";
+import { useEffect } from "react";
+import { VscGithubInverted } from "react-icons/vsc";
+import { api } from "../../services/api";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
 
 export function LoginBox() {
-  const singInUrl = `https//github.com.login/oauth/authorize?scope=user&client_id=0d0cba319108d996d68a`
+  const singInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=cdc0c4aace91395144de`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>("authenticate", {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem("@dowhile:token", token);
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split("?code=");
+
+      window.history.pushState({}, "", urlWithoutCode);
+
+      signIn(githubCode);
+    }
+  }, []);
 
   return (
     <div className={styles.loginBoxWrapper}>
-      <strong>
-        Entre e compartilhe sua mensagem
-      </strong>
-      <a href={singInUrl} className={styles.signInWithGithub} >
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={singInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
     </div>
-  )
+  );
 }
